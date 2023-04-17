c04df0e36c0dcef3b336f043596a541dc9b5622f
VikNord <vikingnorway96@gmail.com>
2023-04-18 00:00:19 +0300

    feat: decide a few task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function createDreamTeam(members) {
+  let name = [];
+    if(Array.isArray(members) == false) {
+      return false}
+  if(members.length == 0){
+    return false
+  }
+    for ( let i = 0; i < members.length; i++) {
+      if(typeof members[i] === 'string') {
+        name.push(members[i].trim())
+      }
+    }
+    name = name.sort((a,b) => a.localeCompare(b));
+
+   let result = "";
+    for (let i = 0; i < name.length; i++){
+      result = result + name[i][0]
+    }
+    return result.toUpperCase();
+  }
 
 module.exports = {
   createDreamTeam
--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,9 +10,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  let emailSeparator = email.lastIndexOf("@");
+  let emailDomain = email.substring(emailSeparator + 1);
+  return emailDomain;
 }
 
 module.exports = {
